7a9cf4532b072f885eca7435ccdd62230a4642e1
Allen <tkmzor@gmail.com>
2025-07-27 21:08:11 +0200

    fix page nav

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -4,7 +4,8 @@ export default function Header() {
   return (
     <header className="bg-gray-900 text-white px-6 py-4 shadow-md">
       <nav className="flex space-x-6">
-        <Link href="/" className="hover:underline">Home</Link>
+        <Link href="/" className="hover:underline">Index</Link>
+        <Link href="/home" className="hover:underline">Home</Link>
         <Link href="/about" className="hover:underline">About</Link>
         <Link href="/posts" className="hover:underline">Posts</Link>
         <Link href="/users" className="hover:underline">Users</Link>
